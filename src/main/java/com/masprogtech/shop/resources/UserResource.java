d5db3a0450fca35c6f14fec0fcacdc55e8e25851
masprog2022 <masprog2022@gmail.com>
2023-01-19 02:29:14 +0100

    :sparkles: feat: Service layer, component registration

--- src/main/java/com/masprogtech/shop/resources/UserResource.java
+++ src/main/java/com/masprogtech/shop/resources/UserResource.java
@@ -1,21 +1,36 @@
 package com.masprogtech.shop.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masprogtech.shop.entities.User;
+import com.masprogtech.shop.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail", "99999999", "12345");
+	@Autowired
+	private UserService userService;
 
-		return ResponseEntity.ok().body(u);
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
+
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = userService.findById(id);
+
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
