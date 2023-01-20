14c5400c1ad45db4c5318e788e84bb0249585c72
masprog2022 <masprog2022@gmail.com>
2023-01-20 12:01:01 +0100

    :sparkles: feat: Payment, one-to-one association

--- src/main/java/com/masprogtech/shop/config/TestConfig.java
+++ src/main/java/com/masprogtech/shop/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.masprogtech.shop.entities.Category;
 import com.masprogtech.shop.entities.Order;
 import com.masprogtech.shop.entities.OrderItem;
+import com.masprogtech.shop.entities.Payment;
 import com.masprogtech.shop.entities.Product;
 import com.masprogtech.shop.entities.User;
 import com.masprogtech.shop.entities.enums.OrderStatus;
@@ -86,6 +87,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 		
